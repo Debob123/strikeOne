0e65e5c96909051fb7c3b2120098af2e48af467c
Noah Caldwell <noah_caldwell2@baylor.edu>
2025-04-05 13:20:52 -0500

    added login.html page

--- app/__init__.py
+++ app/__init__.py
@@ -7,8 +7,6 @@ from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from config import Config
-from app.routes import bp as routes_bp
-from app.models import User
 
 # Initialize libraries
 db = SQLAlchemy()
@@ -46,7 +44,10 @@ def create_app():
     login_manager.init_app(app)
 
     login_manager.login_view = 'routes.login'
-
+    
+    from app.routes import bp as routes_bp
+    from app.models import User
+    
     # Import and register blueprint
     app.register_blueprint(routes_bp)
 
--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,14 @@
-# app/routes.py
-from flask import render_template, redirect, url_for, request, flash
+from flask import render_template, redirect, url_for, request, flash, Blueprint
 from flask_login import login_user, logout_user, login_required, current_user
 from app.models import User
 from app import db, login_manager
-from app.forms import LoginForm  # You'll create this form
+from app.forms import LoginForm  # created form
+
+bp = Blueprint('routes', __name__)
+
+@bp.route('/')
+def homepage():
+    return render_template('homepage.html')
 
 @bp.route('/login', methods=['GET', 'POST'])
 def login():
@@ -15,7 +20,7 @@ def login():
                 flash('Your account has been banned.')
                 return redirect(url_for('routes.login'))
             login_user(user)
-            return redirect(url_for('routes.dashboard'))
+            return redirect(url_for('routes.homepage'))  # Redirect to homepage after login
         else:
             flash('Invalid username or password.')
     return render_template('login.html', form=form)
@@ -24,4 +29,4 @@ def login():
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for('routes.login'))
+    return redirect(url_for('routes.login'))  # Redirect to login page after logout
