984237fa1f98b592563b48f35d6efd885125bb73
Noah Caldwell <noah_caldwell2@baylor.edu>
2025-04-05 12:53:17 -0500

    updated init.py and added functionality to login form

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,7 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from config import Config
 from app.routes import bp as routes_bp
+from app.models import User
 
 # Initialize libraries
 db = SQLAlchemy()
@@ -44,7 +45,13 @@ def create_app():
     bcrypt.init_app(app)
     login_manager.init_app(app)
 
+    login_manager.login_view = 'routes.login'
+
     # Import and register blueprint
     app.register_blueprint(routes_bp)
 
     return app
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
